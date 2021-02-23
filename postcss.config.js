59e239646466079e248d4533021d733c2f7f780b
varuntirumala1 <varun.tirumala@gmail.com>
2021-02-23 17:00:58 -0500

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  plugins: {
-    // add vendor prefixes
-    autoprefixer: { browsers: 'last 2 versions' },
-    // minify the result
-    cssnano: env === 'production' ? {} : false,
-  },
-});
